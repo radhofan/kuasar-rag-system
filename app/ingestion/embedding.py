671140af65215137ce7d8553e838595c3bf8fad7
Radhofan <radhofanazizi@gmail.com>
2025-02-05 13:05:27 +0700

    ada

--- app/ingestion/embedding.py
+++ app/ingestion/embedding.py
@@ -2,23 +2,6 @@ import chromadb
 import uuid 
 import requests
 
-# def get_chroma_client():
-#     client = chromadb.PersistentClient(path="app/data/chromadb")
-#     collection = client.get_or_create_collection("embeddings")
-#     return collection
-
-# def store_embedding_in_chroma(collection, unique_id, embedding, file_name):
-#     try:
-#         collection.add(
-#             documents=[file_name],  
-#             embeddings=[embedding],  
-#             metadatas=[{"source": file_name}],  
-#             ids=[unique_id]  
-#         )
-#         print(f"Embedding stored successfully for {file_name} with ID {unique_id}")
-#     except Exception as e:
-#         print(f"Error storing embedding in Chroma: {e}")
-
 def generate_embedding_with_ollama(text: str):
     try:
         url = "http://localhost:11434/api/embeddings"   
