fa17a970fb6b400dd14fd6ba4b2676dbce7780d2
Radhofan <radhofanazizi@gmail.com>
2025-02-07 09:27:26 +0700

    ada

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,13 @@ import pytest
 
 # Custom Parameters for Pytest
 def pytest_addoption(parser):
-        parser.addoption("--file", action="store", default=None, help="Path to the document file")
+    parser.addoption("--file", action="store", default=None, help="Path to the document file")
     parser.addoption("--question", action="store", default="", help="Question to ask")
 
 @pytest.fixture
 def test_params(request):
-        return {
+    return {
         "file_path": request.config.getoption("--file"),
         "question": request.config.getoption("--question"),
     }
+
